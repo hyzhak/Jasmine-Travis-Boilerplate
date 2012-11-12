b98aaa638f0503a96dafc6cc35824a4941b139c5
hyzhak <hyzhak@ya.ru>
2012-11-12 19:54:13 +0100

    add jasmine adapter for phantomjs

---
+++ spec/index.js
@@ -0,0 +1,26 @@
+(function() {
+    var jasmineEnv = jasmine.getEnv();
+    jasmine.VERBOSE = true;
+    jasmineEnv.updateInterval = 1000;
+
+    var htmlReporter = new jasmine.HtmlReporter();
+
+    jasmineEnv.addReporter(htmlReporter);
+
+    jasmineEnv.specFilter = function(spec) {
+        return htmlReporter.specFilter(spec);
+    };
+
+    var currentWindowOnload = window.onload;
+
+    window.onload = function() {
+        if (currentWindowOnload) {
+            currentWindowOnload();
+        }
+        execJasmine();
+    };
+
+    function execJasmine() {
+        jasmineEnv.execute();
+    }
+})();
